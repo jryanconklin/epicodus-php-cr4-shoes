a5bbda6262a786d2f427d0685b1d782a8a1340a6
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-29 19:17:58 -0700

    Add Initial Function Skeleton to Store and Brand Class.

--- src/Store.php
+++ src/Store.php
@@ -3,35 +3,83 @@
     class Store
     {
 //Properties
-
-
-
+        private $id;
+        private $name;
 
 //Constructor
-
-
-
-
+        function __construct($name, $id = null)
+        {
+            $this->id = $id;
+            $this->name = $name;
+        }
 
 //Getter and Setter Methods
+        function getId()
+        {
 
+        }
 
+        function getName()
+        {
 
+        }
+
+        function setName()
+        {
+
+        }
 
 //Regular Methods
+        function save()
+        {
 
+        }
 
+        function update()
+        {
 
+        }
 
+        function delete()
+        {
 
+        }
 
+        function getJoinList()
+        {
+
+        }
+
+        function addJoinList()
+        {
+
+        }
+
+        function deleteFromJoinList()
+        {
+
+        }
+
+        function deleteAllJoinList()
+        {
+
+        }
 
 //Static Methods
+        function getAll()
+        {
 
+        }
 
+        function deleteAll()
+        {
 
+        }
 
+        function findById()
+        {
 
+        }
 
 //End Class
     }
